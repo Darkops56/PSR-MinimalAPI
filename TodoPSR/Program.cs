e9343b465ea28dc953db29cedec199795ce6c40e
Luis Duran <neoluis87@gmail.com>
2026-06-29 10:15:02 -0300

    Migración de dependencias

--- TodoPSR/Program.cs
+++ TodoPSR/Program.cs
@@ -15,17 +15,15 @@ builder.Services.AddScoped<IDbConnection>(sp => new MySqlConnection(connectionSt
 //Cada vez que necesite la interfaz, se va a instanciar automaticamente AdoDapper y se va a pasar al metodo de la API
 builder.Services.AddScoped<IADO, AdoDapper>();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(options =>
-    {
-        options.RouteTemplate = "/openapi/{documentName}.json";
-    });
+    app.MapOpenApi();
+
+    // 2. Scalar moderno se acopla automáticamente a la ruta configurada arriba
     app.MapScalarApiReference();
 }
 
