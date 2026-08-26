e82e5b1ae7d45dd5e7b73bcdae9766b018604d6c
Sebastian Zerpa <166878493+Darkops56@users.noreply.github.com>
2026-08-26 18:31:10 -0300

    creacion de MVc

---
+++ src/BearPizzeria.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BearPizzeria.Mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
